6e0da8e2b24dabdfb70723aff1da182d65be4a83
Sakura <catarina989sousa@gmail.com>
2024-10-12 01:26:08 +0100

    final changes in banner
    add spline, add icons - need links in icons and animation in all toether

--- my-app/src/components/2-banner/Banner.js
+++ my-app/src/components/2-banner/Banner.js
@@ -3,6 +3,11 @@ import "../2-banner/Banner.css";
 /* other */
 import "../../../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
 
+import Spline from '@splinetool/react-spline';
+import { TypeAnimation } from 'react-type-animation';
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { FaCodepen } from "react-icons/fa";
 
 
 function Banner(props) {
@@ -10,17 +15,26 @@ function Banner(props) {
     <div className="banner" id="home">
       <div className="banner-wrapper">
         <div className="banner-menu-icon">
-        <h2>C</h2><h2>C</h2><h2>C</h2><h2>C</h2>
+          
+        <div className="icon-social"><FaLinkedinIn/></div><div className="icon-social"><FaCodepen/></div ><div className="icon-social"><FiGithub/></div>
         </div>
         <div className="banner-intro">
           <h2> Hello, World! I'm </h2>
-          <h2>Catarina Sousa</h2>
-          <span className="color-intro">Frontend Developer</span>
+          <TypeAnimation
+      sequence={[
+        'Catarina Sousa ',
+        1000, // wait 1s before replacing 
+        'Frontend Developer ',
+        1000]}wrapper="span" speed={50} className="color-intro" repeat={Infinity} />
         </div>
-        <div className="banner-image"></div>
+        <div className="banner-image">
+        <Spline scene="https://prod.spline.design/MGn0zyYNIY2nMpcP/scene.splinecode" />
+        </div>
       </div>
     </div>
   );
 }
 
 export default Banner;
+
+
